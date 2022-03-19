aa95a8f2044f31f5fa77714f103d5eec38860a9e
Jack Barnett <jabarnett@davidson.edu>
2022-03-19 13:36:50 -0400

    Chapter 5

--- Assets/Scripts/LearningCurve.cs
+++ Assets/Scripts/LearningCurve.cs
@@ -4,60 +4,29 @@ using UnityEngine;
 
 public class LearningCurve : MonoBehaviour
 {
-  public int Var1 = 7;
-  public float Var2 = 8.0f;
-  public string FirstName = "Jack";
-  public string LastName = "Barnett";
-  public bool Var4 = true;
-  public int[] arr = {1,2,3};
-  public List<string> lst = new List<string>(5);
-  public Dictionary<string, int> dict = new Dictionary<string, int>();
-  //single line comment
-  /* multi
-  line
-  comment */
-  /// <summary> This is a summary comment.
-  public int Add(int Number)
-  {
-    return Number+1;
-  }
+  public Transform CamTransform;
+  public GameObject DirectionLight;
+  public Transform LightTransform;
 
     // Start is called before the first frame update
     void Start()
     {
-      lst.Add("one");
-      lst.Add("two");
-      dict.Add("one", 1);
-      dict.Add("two", 2);
-      for(int i=0;i<2;i++){
-        Debug.Log(lst[i]);
-      }
-      foreach (string number in lst){
-        Debug.Log($"{number}");
-      }
-      foreach (var i in dict){
-        Debug.LogFormat("Key: {0}, Value: {1}", i.Key, i.Value);
-      }
-      string Name = FirstName + " " + LastName;
-      string greeting = $"Hello, {Name}";
-      Debug.Log(Var1);
-      Debug.LogFormat("Hey this number should be 8: {0}", Var2);
-      Debug.LogFormat("Add one to {0}, and you get {1}", Var1, Add(Var1));
-      if(Var4 == true){
-      }
-      else if(Var4 == !true){
-        if(Var1 == 7){
-        }
-      }
-      else{
-      }
+      CamTransform = this.GetComponent<Transform>();
+      DirectionLight = GameObject.Find("Directional Light");
+      Character hero = new Character("Brad");
+      Character heroine = new Character("Shenine");
+      Weapon huntingBow = new Weapon("Hunting Bow", 105);
+      Paladin knight = new Paladin("Sir Arthur", huntingBow);
+      Weapon warBow = new Weapon("War Bow", 20);
+      hero.PrintStatsInfo();
+      huntingBow.PrintWeaponStats();
+      warBow.PrintWeaponStats();
+      heroine.PrintStatsInfo();
+      knight.PrintStatsInfo();
+      Debug.Log(CamTransform.localPosition);
+      LightTransform = DirectionLight.GetComponent<Transform>();
+      Debug.Log(LightTransform.localPosition);
 
-      switch(Var1){
-        case 3:
-          break;
-        case 7:
-          break;
-        }
     }
 
     // Update is called once per frame
